4c13bf0cfe8678c46bea0474f422d223896d2ffe
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-07 23:21:12 +0100

    Fix task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split(" ", 5)[5].split("language")[0] + "with Python"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
