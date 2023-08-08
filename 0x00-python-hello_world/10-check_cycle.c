4ad2767ef035534afdd4a272441f917413412db0
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-08 02:40:04 +0100

    Add task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,24 @@
 #include "lists.h"
 
-size_t print_listint(const listint_t *h)
-{
-}
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-}
-
-void free_listint(listint_t *head)
-{
-}
-
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the list to be checked
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *tortoise = list;
+	listint_t *hare = list;
+
+	while (hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (hare == tortoise)
+			return (1);
+	}
+
+	return (0);
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,7 +11,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
  */
 typedef struct listint_s
 {
