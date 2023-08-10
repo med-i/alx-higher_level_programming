5afa7c9e94cbdf11d9c68ef098d90822154b8603
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-10 21:06:08 +0100

    Add task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    args_count = len(args)
+
+    if args_count == 1:
+        print("0 arguments.")
+    else:
+        print(f"{args_count - 1} arguments:")
+        for i, arg in enumerate(args[1:], 1):
+            print(f"{i}: {arg}")
