74023be0952e5eade190e37b66179c8db2955cd4
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-10 21:46:16 +0100

    Add task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    numbers = sys.argv
+    result = 0
+
+    for number in numbers[1:]:
+        result += int(number)
+
+    print(result)
