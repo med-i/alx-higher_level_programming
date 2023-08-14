6dde12dc1d27ea77fcb70bee8acfbbfd74e33b0d
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-14 21:56:38 +0000

    Add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-def
+def no_c(my_string):
+    new_string = ""
+    for idx, char in enumerate(my_string):
+        if char in ['c', 'C']:
+            new_string = my_string[:idx] + my_string[idx + 1:]
+    
+    return new_string
