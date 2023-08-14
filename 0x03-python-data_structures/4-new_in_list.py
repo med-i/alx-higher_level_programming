51dab57a17ec734029bd7ab4460cb13910b8034b
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-14 18:39:34 +0100

    Add tasks 3, 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    new_list = list(my_list)
+
+    if idx in range(0, len(new_list)):
+        new_list[idx] = element
+
+    return new_list
