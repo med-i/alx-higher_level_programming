6bd047367d32afd4e7dba5d2cc438804a07a390a
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-15 01:31:06 +0100

    Fix task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,9 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
+
     reversed_list = list(my_list)
     reversed_list.reverse()
 
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
     new_string = ""
     for idx, char in enumerate(my_string):
-        if char in ['c', 'C']:
-            new_string = my_string[:idx] + my_string[idx + 1:]
-    
+        if char in ["c", "C"]:
+            new_string = my_string[:idx] + my_string[idx + 1 :]
+
     return new_string
