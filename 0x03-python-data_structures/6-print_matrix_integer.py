ce7a179ffa43b74f3f98a49753230da7deec4574
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-15 01:55:00 +0100

    Add task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in range(len(row)):
+            end = " " if col < len(row) - 1 else ""
+            print("{:d}".format(row[col]), end=end)
+        print()
