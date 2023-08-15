3e7b416bc639fd4df16c6a947866e1e399f8023b
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-15 03:01:28 +0100

    Add tasks 10, 11, 12

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    multiples_of_two = []
+
+    for num in my_list:
+        multiples_of_two.append(True if num % 2 == 0 else False)
+
+    return multiples_of_two
