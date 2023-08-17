7c2d149fc83c348eb135cf333d83dd11b444d688
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-17 02:33:19 +0100

    Add task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    mul = 0
+    div = 0
+    for tuple in my_list:
+        mul += tuple[0] * tuple[1]
+        div += tuple[1]
+
+    return 0 if div == 0 else mul / div
+
+
+my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+result = weight_average(my_list)
+print("Average: {:0.2f}".format(result))
