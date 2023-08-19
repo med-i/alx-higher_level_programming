4c0ba885d0ff75c95bad062204cd3fb765e50526
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-19 03:48:05 +0100

    Fix task 16

--- 0x04-python-more_data_structures/main_0.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import ctypes
-
-lib = ctypes.CDLL("./libPython.so")
-lib.print_python_list.argtypes = [ctypes.py_object]
-lib.print_python_bytes.argtypes = [ctypes.py_object]
-s = b"Hello"
-lib.print_python_bytes(s)
--- 0x04-python-more_data_structures/main_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import ctypes
-
-lib = ctypes.CDLL("./libPython.so")
-lib.print_python_list.argtypes = [ctypes.py_object]
-lib.print_python_bytes.argtypes = [ctypes.py_object]
-b = b"What does the 'b' character do in front of a string literal?"
-lib.print_python_bytes(b)
