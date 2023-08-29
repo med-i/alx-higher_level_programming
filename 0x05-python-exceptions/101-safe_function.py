5b6d265d12faea1ad907f1954ad3c5d1c401cd90
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-29 03:36:05 +0100

    Add task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    result = None
+    try:
+        result = fct(*args)
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+
+    return result
