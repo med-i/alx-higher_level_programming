e83384709a9d570083554f3c92307c878b0a0058
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-29 03:27:38 +0100

    Add task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as ex:
+        print("Exception: {}".format(ex))
+        return False
