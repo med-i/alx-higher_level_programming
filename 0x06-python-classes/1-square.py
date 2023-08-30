a1f5b781cff50cecfc105f819a73dc7a3b08384c
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-30 02:30:46 +0100

    Add task 0, 1, 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Class that defines a square by: (based on 0-square.py)."""
+
+    def __init__(self, size):
+        """Instantiation with size."""
+        self.__size = size
