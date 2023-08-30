2701f5848c8ce263dc2f8e5f10e4c04ab3e887d0
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-08-30 02:47:59 +0100

    Fix tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
+
 class Square:
-    """Class that defines a square by: (based on 0-square.py)."""
+    """Class that defines a square by: (based on 0-square.py).
+
+    Attributes:
+        __size: The size of the square.
+    """
 
     def __init__(self, size):
-        """Instantiation with size."""
+        """Instantiation with size.
+
+        Args:
+            size (no type/value verification).
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
+
 class Square:
-    """Class that defines a square by: (based on 1-square.py)."""
+    """Class that defines a square by: (based on 1-square.py).
+
+    Attributes:
+        __size(int): The size of the square.
+    """
 
     def __init__(self, size=0):
+        """Instantiate the Square with size."
+
+        Args:
+            __size(int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is negative.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
