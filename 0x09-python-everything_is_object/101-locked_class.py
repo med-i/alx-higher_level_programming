ec60f3e353c47049ba40144637dccf452247ef3c
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-09-05 22:32:21 +0100

    Add task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""This module contains a class that restricts the creation
+of dynamic attributes.
+"""
+
+
 class LockedClass:
+    """_summary_
+
+    Attributes:
+        first_name (str): The first name for class instances.
+    """
+
     __slots__ = ["first_name"]
