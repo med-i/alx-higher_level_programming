3d40205a580247f3257b3e8145a32e2867306450
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-09-05 22:32:51 +0100

    Add task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,7 @@ of dynamic attributes.
 
 
 class LockedClass:
-    """_summary_
+    """A class that restricts dynamic attribute creation.
 
     Attributes:
         first_name (str): The first name for class instances.
