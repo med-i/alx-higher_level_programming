f49849012056d422d6966df89a12beda99b1db40
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-09-13 21:07:42 +0100

    Add task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Demonstrates a custom list with a method to print sorted elements.
+
+Classes:
+    MyList: Inherits from list and adds a method to print sorted elements.
+"""
+
+
+class MyList(list):
+    """
+    A custom list with an added method to print sorted elements.
+
+    Attributes:
+        Inherited from the built-in list class.
+
+    Methods:
+        print_sorted: Prints the elements of the list in sorted order.
+    """
+
+    def print_sorted(self):
+        """Prints the elements of the list in sorted order."""
+        sorted_list = sorted(self)
+        print(sorted_list)
