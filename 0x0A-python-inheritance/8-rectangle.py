6553354f336ff401cc13b2001d100515b46b0d0d
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-09-19 22:45:00 +0100

    Add task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines the Rectangle class.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle class inheriting from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initializes the rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
