1002fde60fbbef05b7d7abf45a9e8f197693ca1b
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-15 02:51:52 +0100

    Fix tasks 2, 5, 8, 14, and 15

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,5 @@
 -- displays the number of records with id = 89 in the table first_table
 -- of the database hbtn_0c_0 in MySQL server.
-SELECT * FROM first_table
+SELECT COUNT(*)
+FROM first_table
 WHERE id = 89;
