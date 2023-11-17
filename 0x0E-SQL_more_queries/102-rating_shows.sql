77f54908fd813bc2b981fa084ad7c4ab30447eb1
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-17 01:40:38 +0100

    Add advanced tasks

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
