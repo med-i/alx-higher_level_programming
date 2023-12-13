ed86ed46c042a0c4d22e76f6c32703e22202e754
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-12-13 22:14:52 +0100

    Add advanced tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { dict } = require('./101-data');
+
+const newDict = {};
+
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value] === undefined) {
+    newDict[value] = [];
+  }
+
+  newDict[value].push(key);
+}
+
+console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+function readFile (file) {
+  let text = '';
+
+  try {
+    text = fs.readFileSync(file);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return text;
+}
+
+const args = process.argv.slice(2);
+const fileA = readFile(args[0]);
+const fileB = readFile(args[1]);
+
+try {
+  fs.writeFileSync(args[2], fileA);
+  fs.appendFileSync(args[2], fileB);
+} catch (err) {
+  console.error(err);
+}
