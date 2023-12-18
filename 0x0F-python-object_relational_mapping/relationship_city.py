bc4639de7cd18a49e94d4d9952896afceb60fe33
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-12-18 02:35:28 +0100

    Add task 15

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This file contains the class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """
+    The City class.
+    """
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+This file contains the class definition of a State.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This State class.
+    """
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan"
+    )
