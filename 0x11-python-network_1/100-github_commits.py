1b1ce5a927ca26938f56194087d195766eb5e9cf
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-07 05:05:09 +0100

    Add task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 """
-Task 10
+This script lists 10 last commits of a Github repo.
 """
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author_name}")
