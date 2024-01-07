83a36976e4844349e73ffa7a8189680ea27f67cd
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-07 04:46:31 +0100

    Add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,13 @@ import sys
 import requests
 
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+    print(
+        response.json().get("id")
+        if response.status_code == 200
+        else None
+    )
