8a42c303d1dbc557f887b7ed3ebd3b0faceb7b08
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-31 20:33:06 +0100

    Add task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2];
+const string = process.argv[3];
+
+if (!file || !string) {
+  console.error(`Error: ${!file ? 'No file path provided' : 'No string provided'}`);
+  process.exit(1);
+}
+
+fs.writeFile(file, string, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
