8f1d36b425cc7227714383d6a61e99f88fdd84cb
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-02-01 01:34:34 +0100

    Add task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (completedTasks[todo.userId]) {
+          completedTasks[todo.userId] += 1;
+        } else {
+          completedTasks[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasks);
+  } else {
+    console.error('Failed to retrieve data:', error);
+  }
+});
