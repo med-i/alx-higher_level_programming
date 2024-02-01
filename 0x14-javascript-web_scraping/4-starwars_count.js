6a52c61f7ac418b9fa25a99413b6c50a418907b5
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-02-01 01:23:21 +0100

    Add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach(film => {
+      const characters = film.characters;
+      if (characters.some(characterUrl => characterUrl.includes(`/people/${characterId}/`))) {
+        count++;
+      }
+    });
+    console.log(count);
+  } else {
+    console.error('Failed to retrieve data:', error);
+  }
+});
