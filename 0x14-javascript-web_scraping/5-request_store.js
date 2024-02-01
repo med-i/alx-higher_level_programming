f9452cb611192cedebd0473346a89732b5ba3f21
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-02-01 01:27:28 +0100

    Add task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(file, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } else {
+    console.error('Failed to retrieve data:', error);
+  }
+});
