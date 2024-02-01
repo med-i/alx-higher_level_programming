7f355a5f9510ef438c5f0d67c14e897545f3b78a
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-02-01 01:43:56 +0100

    Add task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(character => {
+      request(character, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  } else {
+    console.error('Failed to retrieve movie data:', error);
+  }
+});
